e047c76e5e70a5185ce3e4167b60656fae5c7737
昌维 <867597730@qq.com>
2021-04-09 18:40:23 +0800

    update Home page: add English description on Experience section. add copyright and last update datetime on footer.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -27,7 +27,7 @@ export default function Home(props: IPropsHome) {
         I hope to make a contribution to the popularization of computer science and electronic.
       </p>
       <p>
-        <h2>ECE</h2>
+        <h2>ECE (ElectroniC Engineering)</h2>
         <ul>
           <li>嵌入式单片机开发，包括 STM32，MSC-51，ESP8266(NodeMCU) 等平台。</li>
           <li>Embedded development, including STM32，MSC-51，ESP8266(NodeMCU) and more platform.</li>
@@ -36,7 +36,7 @@ export default function Home(props: IPropsHome) {
         </ul>
       </p>
       <p>
-        <h2>CS</h2>
+        <h2>CS (Computer Science)</h2>
         <ul>
           <li>WEB全栈开发，泛语言开发者，目前正在进行中的项目请参考本人 GitHub，Username 为 cw1997。</li>
           <li>WEB Full-stack developer, you can see my project on my GitHub what the username is 'cw1997'</li>
@@ -44,19 +44,19 @@ export default function Home(props: IPropsHome) {
       </p>
       <p>
         <table border="1" cellspacing="0">
-          <caption>个人经历</caption>
+          <caption>Experience (个人经历)</caption>
           <thead>
             <tr>
-              <th>时间</th><th>组织机构</th><th>职位方向</th><th>性质</th>
+              <th>Date (时间)</th><th>Organization (组织机构)</th><th>Position (职位方向)</th><th>Nature (性质)</th>
             </tr>
             <tr>
-              <td>2020.10 ~ 2021.3</td><td>PingCAP (北京平凯星辰科技发展有限公司)</td><td>WEB Front-End (WEB前端开发)</td><td>intern (实习)</td>
+              <td>2020.10 ~ 2021.3</td><td><a href="https://pingcap.com">PingCAP</a> (北京平凯星辰科技发展有限公司)</td><td>WEB Front-End (WEB前端开发)</td><td>intern (实习)</td>
             </tr>
             <tr>
-              <td>2018.9 ~ 至今</td><td>NTUST (台湾科技大学)</td><td>ElectriC Engineering (ECE) (电子工程)</td><td>full-time undergraduate (全日制本科)</td>
+              <td>2018.9 ~ 至今</td><td><a href="https://www.ntust.edu.tw/">NTUST</a> (台湾科技大学)</td><td>ElectriC Engineering (ECE) (电子工程)</td><td>full-time undergraduate (全日制本科)</td>
             </tr>
             <tr>
-              <td>2015.9 ~ 2018.9</td><td>WSPC (武汉船舶职业技术学院)</td><td>Software Technology (软件技术)</td><td>full-time college (全日制专科)</td>
+              <td>2015.9 ~ 2018.9</td><td><a href="http://www.wspc.edu.cn/">WSPC</a> (武汉船舶职业技术学院)</td><td>Software Technology (软件技术)</td><td>full-time college (全日制专科)</td>
             </tr>
           </thead>
         </table>
@@ -65,8 +65,10 @@ export default function Home(props: IPropsHome) {
         <h2>联系方式(Contact)</h2>
         <ul>
           <li>Github: <a href="https://github.com/cw1997">https://github.com/cw1997</a></li>
-          <li>zhihu: <a href="https://www.zhihu.com/people/changwei1006/posts">https://www.zhihu.com/people/changwei1006/posts</a></li>
-          <li>weibo: <a href="https://weibo.com/changweicw">https://weibo.com/changweicw</a></li>
+          <li>ZhiHu: <a href="https://www.zhihu.com/people/changwei1006/posts">https://www.zhihu.com/people/changwei1006/posts</a></li>
+          <li>Weibo: <a href="https://weibo.com/changweicw">https://weibo.com/changweicw</a></li>
+          <li>SegmentFault: <a href="https://segmentfault.com/u/changwei">https://segmentfault.com/u/changwei</a></li>
+
           <li>QQ: 867597730</li>
           <li>WeChat: changwei1006</li>
         </ul>
@@ -81,6 +83,9 @@ export default function Home(props: IPropsHome) {
         </p>
         <p>
           Powered by React.js, SSR by React-DOM-Server & koa2, running at CentOS with Docker.
+        </p>
+        <p>
+          Copyright 2020 - 2021 cw1997. Last Update DateTime: Fri Apr 09 2021 18:32:19 GMT+0800 (中国标准时间)
         </p>
       </footer>
     </div>
