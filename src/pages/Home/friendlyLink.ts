7faaaec39eb2f7af9b3d829a7f10b56f10d164e8
昌维 <867597730@qq.com>
2021-04-10 17:48:22 +0800

    update Home page: add some friendly links

--- src/pages/Home/friendlyLink.ts
+++ src/pages/Home/friendlyLink.ts
@@ -5,7 +5,14 @@ export interface IFriendlyLinkItem {
 }
 
 const friendlyLink: IFriendlyLinkItem[] = [
-  {name: '昌维的博客', url: 'https://blog.changwei.me/'},
+  {name: `昌维的博客`, url: `https://blog.changwei.me/`},
+  {name: `zhangguanzhang's Blog`, url: `https://zhangguanzhang.github.io/`},
+  {name: `猫叔's Blog`, url: `https://www.vicicode.com/`},
+  {name: `Ah!'s Blog`, url: `https://blog.forgiveher.cn/`},
+  {name: `劣質文章農場`, url: `https://blog.splitline.tw/`},
+  {name: `小青青的秘密基地`, url: `http://largeq.cn/`},
+  {name: `Leo's Blog`, url: `https://freeleox.github.io/`},
+  {name: `Cytosineの有被小窝~`, url: `https://cyto.top/`},
 ]
 
 export default friendlyLink
