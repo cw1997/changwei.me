22cbbda9d85812cc0cc299ad294ac804be17256c
Chang Wei <changwei1006@gmail.com>
2022-08-27 03:05:22 +0800

    update data：update 'Last Update DateTime'
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -156,7 +156,7 @@ export default function Home(props: IPropsHome) {
           Powered by React.js & TypeScript & sass & Webpack, SSR (Server Side Rendering) by React-DOM-Server & koa2, running at CentOS with nginx in Docker, build on GitHub Actions.
         </p>
         <p>
-          Copyright 2020 - 2022 cw1997. Last Update DateTime: 2022-08-08 01:02:28 ( UTC+8 中国标准时间 )
+          Copyright 2020 - 2022 cw1997. Last Update DateTime: 2022-08-27 03:04:52 ( UTC+8 中国标准时间 )
         </p>
         <p>
           <a href="https://beian.miit.gov.cn/">赣ICP备15009162号-1</a>
