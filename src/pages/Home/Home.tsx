c40549b97f850962683cdecc6b4611b960fd0fb2
Chang Wei <changwei1006@gmail.com>
2022-08-27 03:04:22 +0800

    update data: remove redundant content
    Signed-off-by: Chang Wei <changwei1006@gmail.com>

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -95,7 +95,7 @@ export default function Home(props: IPropsHome) {
               <th>Date (时间)</th><th>Organization (组织机构)</th><th>Position (职位方向)</th><th>Nature (性质)</th><th>Duty (方向与职责)</th>
             </tr>
             <tr>
-              <td>2018.9 ~ Now</td><td><a href="https://www.ntust.edu.tw/">NTUST, National Taiwan University of Science and Technology</a> (National Taiwan University of Science and Technology, 台湾科技大学)</td><td>Electronic Engineering (电子工程)</td><td>full-time master (全日制硕士)</td>
+              <td>2018.9 ~ Now</td><td><a href="https://www.ntust.edu.tw/">NTUST, National Taiwan University of Science and Technology</a> (台湾科技大学)</td><td>Electronic Engineering (电子工程)</td><td>full-time master (全日制硕士)</td>
               <td>台科大电子工程研究所丙组(光电及半导体组)</td>
             </tr>
             <tr>
